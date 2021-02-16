6d6594ffd8502051722cedd772614ff722a4d750
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-02-16 20:19:37 -0300

    Example promse race

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { promiseFast, promiseSlow, promiseMedium } from './js/promesas.js'
 
-promiseSlow.then(console.log);
+/* promiseSlow.then(console.log);
 promiseMedium.then(console.log);
-promiseFast.then(message => console.log(message));
+promiseFast.then(message => console.log(message)); */
+
+/* Promise.race([promiseFast, promiseSlow, promiseMedium])
+    .then(message => console.log(message)); */
+
+Promise.race([promiseFast, promiseSlow, promiseMedium])
+    .then(console.log);

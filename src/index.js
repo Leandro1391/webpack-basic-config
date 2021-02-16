dbba01be63d893a73af196aac041d13a4fcabaaa
Leandro Iadanza <leandrojulian1391@gmail.com>
2021-02-16 20:20:21 -0300

    Comment in promise .race

--- src/index.js
+++ src/index.js
@@ -7,5 +7,8 @@ promiseFast.then(message => console.log(message)); */
 /* Promise.race([promiseFast, promiseSlow, promiseMedium])
     .then(message => console.log(message)); */
 
+
+// En promise race va invocar solamente la promsea más rápda, si alguna de las otra da un error no se va a mostrar
+
 Promise.race([promiseFast, promiseSlow, promiseMedium])
     .then(console.log);
